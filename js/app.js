87ee315a3a80a09b06de62a3b1e23d7e4b15d237
Aldi-Fz99 <aldifahroza444@gmail.com>
2024-12-25 04:40:00 +0700

    feat: update js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// app.js
+// anavbar setting serponsice start
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
@@ -27,3 +27,28 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
+// navbar end
+
+// slider colection digimon start
+var TrandingSlider = new Swiper(".tranding-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// slider colection digimon end
