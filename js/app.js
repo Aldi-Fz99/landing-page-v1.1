7f28e99e525c036b215efcecb77c3f1a3f0d604f
Aldi-Fz99 <aldifahroza444@gmail.com>
2024-12-31 10:14:24 +0700

    fix: js

--- js/app.js
+++ js/app.js
@@ -52,3 +52,51 @@ var TrandingSlider = new Swiper(".tranding-slider", {
   },
 });
 // slider colection digimon end
+
+// movie animation start
+let nextButton = document.getElementById("next");
+let prevButton = document.getElementById("prev");
+let backButton = document.getElementById("goback");
+let seeMoreButtons = document.querySelectorAll(".seeMore");
+let carousel = document.querySelector(".carousel");
+let listHtml = document.querySelector(".carousel .list");
+
+nextButton.onclick = function () {
+  showSlider("next");
+};
+prevButton.onclick = function () {
+  showSlider("prev");
+};
+
+let unAcceptClick;
+
+const showSlider = (type) => {
+  prevButton.style.pointerEvents = "none";
+  nextButton.style.pointerEvents = "none";
+
+  carousel.classList.remove("prev", "next");
+  let items = document.querySelectorAll(".carousel .list .item");
+  if (type === "next") {
+    listHtml.appendChild(items[0]);
+    carousel.classList.add("next");
+  } else {
+    let positionLast = items.length - 1;
+    listHtml.prepend(items[positionLast]);
+    carousel.classList.add("prev");
+  }
+  clearTimeout(unAcceptClick);
+  unAcceptClick = setTimeout(() => {
+    prevButton.style.pointerEvents = "auto";
+    nextButton.style.pointerEvents = "auto";
+  }, 2000);
+};
+
+seeMoreButtons.forEach((button) => {
+  button.onclick = function () {
+    carousel.classList.add("showDetail");
+  };
+});
+backButton.onclick = function () {
+  carousel.classList.remove("showDetail");
+};
+// movie animation end
